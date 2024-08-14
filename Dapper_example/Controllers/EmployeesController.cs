afa0b3a2e7d725a7141cb9da09ea2067d8fd4146
Salomao Ferreira <salomao1991@gmail.com>
2024-08-14 14:53:32 -0400

    replace base controller methods to asynchronous methods

--- Dapper_example/Controllers/EmployeesController.cs
+++ Dapper_example/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper_example.Models;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +9,44 @@ namespace Dapper_example.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+        public EmployeesController()
+        {
+
+        }
+
         // GET: api/<EmployeesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> GetAll()
         {
-            return new string[] { "value1", "value2" };
+            return Ok();
         }
 
         // GET api/<EmployeesController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetById(int id)
         {
-            return "value";
+            return Ok();
         }
 
         // POST api/<EmployeesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post(EmployeeModel model)
         {
+            return NoContent();
         }
 
         // PUT api/<EmployeesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id, EmployeeModel model) 
         {
+            return NoContent();
         }
 
         // DELETE api/<EmployeesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            return NoContent();
         }
     }
 }
